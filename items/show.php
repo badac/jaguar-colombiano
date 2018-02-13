0ecb65231742dead966f0d51717176b814472266
decolector <cmart@decolector.net>
2018-02-13 17:06:40 -0500

    typo rhythm, item show metadata markup and styles

--- items/show.php
+++ items/show.php
@@ -20,7 +20,6 @@ if ($hasImages) {
 echo head(array('title' => metadata('item', array('Dublin Core', 'Title')), 'bodyclass' => 'items show'));
 ?>
 
-<h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
 
 <!-- The following returns all of the files associated with an item. -->
 <?php if ($hasImages): ?>
@@ -40,21 +39,38 @@ echo head(array('title' => metadata('item', array('Dublin Core', 'Title')), 'bod
     <?php endforeach; ?>
 <?php endif; ?>
 
-<?php echo all_element_texts('item'); ?>
+<h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
+
+<?php echo all_element_texts('item', array('partial','common/record-metadata.php')); ?>
+
 
 <!-- If the item belongs to a collection, the following creates a link to that collection. -->
 <?php if (metadata('item', 'Collection Name')): ?>
-<div id="collection" class="element">
-    <h3><?php echo __('Collection'); ?></h3>
-    <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
+<div id="collection">
+    <dl class="element">
+      <dt>
+        <?php echo __('Collection'); ?>
+      </dt>
+      <dd>
+        <div class="element-text">
+          <?php echo link_to_collection_for_item(); ?>
+        </div>
+      </dd>
+    </dl>
 </div>
 <?php endif; ?>
 
 <!-- The following prints a list of all tags associated with the item -->
 <?php if (metadata('item', 'has tags')): ?>
 <div id="item-tags" class="element">
-    <h3><?php echo __('Tags'); ?></h3>
-    <div class="element-text"><?php echo tag_string('item'); ?></div>
+  <dl class="element" id="item-tags">
+    <dt>
+      <?php echo __('Tags'); ?>
+    </dt>
+    <dd>
+      <div class="element-text"><?php echo tag_string('item'); ?></div>
+    </dd>
+  </dl>
 </div>
 <?php endif;?>
 
@@ -69,17 +85,17 @@ echo head(array('title' => metadata('item', array('Dublin Core', 'Title')), 'bod
 
 <!-- The following prints a citation for this item. -->
 <div id="item-citation" class="element">
-    <h3><?php echo __('Citation'); ?></h3>
-    <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
+    <h3><?php echo __('Citation:'); ?></h3>
+    <div class="free-element"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
 </div>
 
 <div id="item-output-formats" class="element">
-    <h3><?php echo __('Output Formats'); ?></h3>
-    <div class="element-text"><?php echo output_format_list(); ?></div>
+    <h3><?php echo __('Output Formats:'); ?></h3>
+    <div class="free-element"><?php echo output_format_list(); ?></div>
 </div>
-
+<!--
 <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
-
+-->
 <nav>
 <ul class="item-pagination navigation">
     <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
